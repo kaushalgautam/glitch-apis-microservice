8e6fc65c69c7591ba49fec38e3e76c5eb8814605
Glitch (fish-attempt) <none>
2019-06-04 08:57:50 +0000

    👮✊ Updated with Glitch

--- myApp.js
+++ myApp.js
@@ -60,7 +60,9 @@ app.get('/now', (req, res, next) => {
 })
 
 /** 9)  Get input from client - Route parameters */
-
+app.get('/:word/echo', (req, res) => {
+  res.json({word: req.params.word});
+})
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
